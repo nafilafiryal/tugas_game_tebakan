23b5eec54300c27af9abb09c5ccd83e8bb73b06d
nafilafiryal <nafilahfiryal05@gmail.com>
2026-03-09 23:00:35 +0700

    Commit 2: Menambahkan angka acak dan perulangan while

--- main/bin/main.dart
+++ main/bin/main.dart
@@ -1,5 +1,21 @@
-import 'package:main/main.dart' as main;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${main.calculate()}!');
+void main() {
+  print("=== GAME TEBAK ANGKA ===");
+  int angkaRahasia = Random().nextInt(100) + 1; // Bikin angka acak 1-100
+  int tebakan = 0;
+
+  print("Aku sudah memilih angka 1 sampai 100. Coba tebak!");
+
+  // Perulangan while: akan terus diulang selama tebakan salah
+  while (tebakan != angkaRahasia) {
+    stdout.write("Masukkan tebakanmu: ");
+    String? input = stdin.readLineSync();
+
+    if (input != null) {
+      tebakan = int.parse(input);
+    }
+  }
+  print("Selamat! Kamu berhasil menebak angka $angkaRahasia.");
 }
